d968b087e94c8927d56a870bdff4d23d59474a5f
rotza1802 <65125661+rotza1802@users.noreply.github.com>
2021-08-20 15:01:05 -0300

    Se comento el midleware del token

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -3,9 +3,9 @@ const productRouter = express.Router();
 const productController = require("../controllers/productController");
 const checkJwt = require("express-jwt");
 
-productRouter.use(
+/* productRouter.use(
   checkJwt({ secret: process.env.TOKEN_STRING_SECRETO, algorithms: ["HS256"] })
-);
+); */
 productRouter.get("/:id", productController.show);
 productRouter.patch("/:id", productController.update);
 productRouter.post("/", productController.create);
