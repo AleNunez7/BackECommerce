f32bf7920b64d2a5425254b9b966d71af5223835
rotza1802 <65125661+rotza1802@users.noreply.github.com>
2021-09-01 12:29:37 -0300

    Se creo contador para el numero de orden

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,8 +1,9 @@
 const { Order } = require("../models/Order");
 const { Product } = require("../models/Product");
-
+let counter = 0;
 async function createOrder(req, res) {
   const order = new Order({
+    orderNumber: counter++,
     user: req.body.user._id,
     orderList: req.body.cart,
     totalPrice: req.body.total,
--- models/Order.js
+++ models/Order.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
+  orderNumber: Number,
   user: { type: Schema.Types.ObjectId, ref: "User" },
   orderList: [],
   totalPrice: String,
